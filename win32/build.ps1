9169f185e920205362b9da66b90af8204fecb8af
Heiko Panjas <heiko@panjas.com>
2020-03-06 16:18:23 +0100

    Update for RC3

--- win32/build.ps1
+++ win32/build.ps1
@@ -126,7 +126,7 @@ if ($BuildFailed -eq $False) {
 if ($BuildFailed -eq $False) {
   Push-Location $PluginDirectory
   if ($BuildRelease -eq $True) {
-    $BuildId = "4.0-RC2"
+    $BuildId = "4.0-RC3"
   }
   Else {
     $BuildId = (git describe --tags | Out-String).Trim()
