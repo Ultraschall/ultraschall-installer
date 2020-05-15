81bbac6dfd61ae293c70939beb58bd9f73b633e9
Heiko Panjas <heiko@panjas.com>
2020-05-16 00:10:36 +0200

    Enable incremental build

--- linux/installer-scripts/install.sh
+++ linux/installer-scripts/install.sh
@@ -5,8 +5,33 @@ if [ -d "$HOME/.config/REAPER" ]; then
   BACKUP_FOLDER="$HOME/.config/ultraschall/backups/$BACKUP_TIMESTAMP"
   mkdir -p "$BACKUP_FOLDER"
   cp -r "$HOME/.config/REAPER" "$BACKUP_FOLDER"
+  if [ $? -eq 0 ]; then
+    echo "Your current REAPER configuration has been save to $BACKUP_FOLDER."
+  else
+    echo "Failed to backup your current REAPER configuration. Aborting installation..."
+    exit -1
+  fi
 fi
 
+echo "Installing the Ultraschall REAPER Theme..."
 unzip -o ./themes/Ultraschall_4.0.ReaperConfigZip -d "$HOME/.config/REAPER"
-cp -fr ./plugins/* "$HOME/.config/REAPER/UserPlugins"
-cp -fr ./scripts/* "$HOME/.config/REAPER/Scripts"
+if [ $? -eq 0 ]; then
+    echo "Done."
+    echo "Installing the Ultraschall REAPER Plug-ins..."
+    cp -fr ./plugins/* "$HOME/.config/REAPER/UserPlugins"
+    if [ $? -eq 0 ]; then
+      echo "Done."
+      echo "Installing the Ultraschall REAPER Scripts..."
+      cp -fr ./scripts/* "$HOME/.config/REAPER/Scripts"
+      if [ $? -eq 0 ]; then
+        echo "Done."
+        exit 0
+      fi
+    fi
+  fi
+fi
+
+if [ $? -ne 0 ]; then
+  echo "Failed to install Ultraschall."
+  exit -1
+fi
