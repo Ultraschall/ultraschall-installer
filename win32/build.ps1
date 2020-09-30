9f6ca839a459c0a2d864ec6342e80a7e74855753
Heiko Panjas <heiko@panjas.com>
2020-09-30 13:22:56 +0200

    Update for V5.0.0

--- win32/build.ps1
+++ win32/build.ps1
@@ -23,7 +23,7 @@ if ($BuildRelease -eq $True) {
 }
 
 $BuildDirectory = "./build"
-$BuildId = "R4.1.0_GENERIC"
+$BuildId = "R5.0.0_GENERIC"
 $BuildFailed = $False
 
 if ((Test-Path -PathType Container $BuildDirectory) -eq $False) {
@@ -131,7 +131,7 @@ if ($BuildFailed -eq $False) {
 if ($BuildFailed -eq $False) {
   Push-Location $PluginDirectory
   if ($BuildRelease -eq $True) {
-    $BuildId = "4.1.0"
+    $BuildId = "5.0.0"
   }
   Else {
     $BuildId = (git describe --tags | Out-String).Trim()
