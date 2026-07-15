045d011888fd802fc0efc2dbe21f297d354b3e1e
udo <udo@udosauer.de>
2026-07-15 19:20:47 +0200

    set special portable branch for testing

--- linux/build.sh
+++ linux/build.sh
@@ -54,7 +54,7 @@ ULTRASCHALL_SOUNDBOARD_URL="https://github.com/Ultraschall/ultraschall-soundboar
 ULTRASCHALL_SOUNDBOARD_BRANCH="main"
 
 ULTRASCHALL_PORTABLE_URL="https://github.com/Ultraschall/ultraschall-portable.git"
-ULTRASCHALL_PORTABLE_BRANCH="master"
+ULTRASCHALL_PORTABLE_BRANCH="5_1_2-Update-auf-Reaper-7.77"
 
 ULTRASCHALL_ASSETS_URL="https://github.com/Ultraschall/ultraschall-assets.git"
 ULTRASCHALL_ASSETS_BRANCH="master"
